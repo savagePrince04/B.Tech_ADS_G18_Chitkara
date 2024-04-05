336e9b85d683a4c0710f0b5422eb9346e6f4512a
Sudhakar Kumar <91364666+savagePrince04@users.noreply.github.com>
2024-04-06 00:59:41 +0530

    Update Binary_Tree_Client.java

--- Lec6/Binary_Tree_Client.java
+++ Lec6/Binary_Tree_Client.java
@@ -5,7 +5,6 @@ import Lec6.Binary_Tree;
 public class Binary_Tree_Client {
 
     public static void main(String[] args) {
-// 10 true 20 true 40 false false true 50 false false true 30 false true 60 true 70 false false false
         Binary_Tree bt = new Binary_Tree();
         bt.PreOrder();
         bt.InOrder();
@@ -13,3 +12,8 @@ public class Binary_Tree_Client {
     }
 
 }
+
+
+
+// you can give the input in this formate (input formate are give below)
+// 10 true 20 true 40 false false true 50 false false true 30 false true 60 true 70 false false false
