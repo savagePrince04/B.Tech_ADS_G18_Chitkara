24ef51f5d20ab5434af94674c5d51d4fbacfcbe0
Sudhakar Kumar <91364666+savagePrince04@users.noreply.github.com>
2024-05-04 10:42:15 +0530

    Add files via upload

---
+++ Lec25/Gragp_client.java
@@ -0,0 +1,33 @@
+package Lec25;
+
+public class Gragp_client {
+    public static void main(String[] args) {
+        Graph g=new Graph(6);
+        g.AddEdge(1,2,6);
+        g.AddEdge(1,3,7);
+        g.AddEdge(3,4,8);
+        g.AddEdge(4,2,5);
+        g.AddEdge(4,5,1);
+        g.AddEdge(2,5,2);
+        g.AddEdge(5,6,3);
+        g.display();
+        System.out.println("********************");
+        g.RemoveEdge(1,3);
+        g.display();
+        System.out.println(g.ContainsEdge(1,3));
+    g.AddVertex(10);
+        g.display();
+        g.RemoveVertex(10);
+        g.display();
+        System.out.println("*************");
+       boolean a= g.BFS(1,2);
+        System.out.println(a);
+        g.display();
+        boolean a2= g.BFS(1,6);
+        System.out.println(a2);
+
+        System.out.println(g.DFS(1,6));
+        System.out.println(g.DFS(2,6));
+
+    }
+}
---
+++ Lec25/Graph.java
@@ -0,0 +1,93 @@
+package Lec25;
+import java.util.*;
+
+public class Graph {
+    HashMap<Integer,HashMap<Integer,Integer>> map=new HashMap<>();
+    Graph(int v){
+        for(int i=1;i<=v;i++){
+            map.put(i,new HashMap<>());
+        }
+    }
+    public void AddEdge(int v1,int v2,int cost){
+        map.get(v1).put(v2,cost);
+        map.get(v2).put(v1,cost);
+    }
+    public void AddVertex(int v){
+        map.put(v,new HashMap<>());
+    }
+    public int NooFEdge(){
+        int sum=0;
+        for(int nbrs:map.keySet()){
+            sum=sum+map.get(nbrs).size();
+        }
+        return sum/2;
+    }
+    public boolean ContainsEdge(int v1,int v2){
+         return map.get(v1).containsKey(v2);
+    }
+    public void RemoveEdge(int v1,int v2){
+        map.get(v1).remove(v2);
+        map.get(v2).remove(v1);
+    }
+    public void RemoveVertex(int v){
+        for(int nbrs: map.keySet()){
+            map.get(nbrs).remove(v);
+        }
+        map.remove(v);
+    }
+    public void display(){
+        for(int key: map.keySet()){
+            System.out.println(key+"="+map.get(key));
+        }
+    }
+    public boolean BFS(int src, int des) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+        HashSet<Integer> visited = new HashSet<>();
+        while (!q.isEmpty()) {
+            // Remove
+            int rv = q.remove();
+            // ignore
+            if (visited.contains(rv)) {
+                continue;
+            }
+            // Add visited
+            visited.add(rv);
+            // self work
+            if (rv == des) {
+                System.out.print(rv+" ");
+                return true;
+            }
+            // add Unvisited
+            for (int nbrs : map.get(rv).keySet()) {
+                if (!visited.contains(nbrs)) {
+                    q.add(nbrs);
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean DFS(int src, int des) {
+        Stack<Integer> s = new Stack<>();
+        s.push(src);
+        HashSet<Integer> visited = new HashSet<>();
+        while (!s.isEmpty()) {
+            int rv = s.pop();
+            if (visited.contains(rv)) {
+                continue;
+            }
+            visited.add(rv);
+            if (rv == des) {
+                System.out.print(rv+" ");
+                return true;
+            }
+            for (int nbrs : map.get(rv).keySet()) {
+                if (!visited.contains(nbrs)) {
+                    s.push(nbrs);
+                }
+            }
+        }
+        return false;
+    }
+}
